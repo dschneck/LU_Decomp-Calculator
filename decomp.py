a8db5665f593e2617db562d68bcc4ee5bf8f122f
David Schneck <david.schneckster@hotmail.com>
2021-10-20 18:37:45 -0400

    removed module cause its not working

--- decomp.py
+++ decomp.py
@@ -5,19 +5,20 @@ sys.path.insert(0, '../RREF-calculator')
 from matrix import Matrix
 from rref import RREF
 
+'''
 class Decomp:
 	def __init__(self, A: Matrix):
 		if A.n != A.m:
 			print("Not a square matrix, LU Decomp unavailable")
 
+'''
 if __name__ == "__main__":
+	print("hellO")
 	x = Matrix([[1,1],[1,1]],2,2)
 	x.printMatrix()
 	y = RREF(x)
 
-	'''
-	if getattr(y, 'flag') == True:
+	if y.flag == True:
 		print("I had to interchange")
 	else:
 		print("I did not have to interchange")
-	'''
